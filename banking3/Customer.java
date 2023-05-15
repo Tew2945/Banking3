ae0ca85afa7afc7309b803f724664ffabfa6b163
Tew2945 <teeratorn.thie@mail.kmutt.ac.th>
2023-05-15 17:11:06 +0700

    Banking

---
+++ banking3/Customer.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banking3;
+
+public class Customer {
+    int idCard;
+    String fistName;
+    String lastName;
+    String phoneNumber;
+
+    public int getIdCard() {
+        return idCard;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+}
